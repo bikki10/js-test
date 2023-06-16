9caacd48c9c0deb19bda9cb2116f9f5c903908ab
Bikram Bhusal <bikrambhusal75@gmail.com>
2023-06-16 14:15:53 +0545

    learned about logical operators

--- index.js
+++ index.js
@@ -7,11 +7,31 @@
 // var c = "abc";
 // console.log(c);
 
-const greet = alert("Hello mate!");
-console.log(greet);
+// const greet = alert("Hello mate!");
+// console.log(greet);
 
-const age = prompt("How old are you?");
-console.log(age);
+// const age = prompt("How old are you?");
+// console.log(age);
 
-let sure = confirm("Are you sure?");
-console.log(sure);
+// let sure = confirm("Are you sure?");
+// console.log(sure);
+
+let x = 1;
+let y = 2;
+
+if(x==1)
+{
+    console.log("x is 1");
+}
+
+if(x===1 && y ===2){
+    console.log("&& operator tested");
+}
+
+if(( x=1 ) || (y = 3)){
+    console.log("|| operator tested");
+}
+
+if(!x){
+    console.log("x is not correct!");
+}
