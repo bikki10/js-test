df45679ec9fcb6390bf3558d48799301e63d1444
Bikram Bhusal <bikrambhusal75@gmail.com>
2023-06-23 13:32:32 +0545

    Weather data fatching

--- node-test/qr_code.js
+++ node-test/qr_code.js
@@ -6,8 +6,46 @@
 //   console.log(url)
 // })
 
-const QRCode = require('qrcode')
+// const QRCode = require('qrcode')
 
-QRCode.toString('Bikki is a damn good developer!',{type:'terminal'}, function (err, url) {
-  console.log(url)
-})
+// QRCode.toString('Bikki is a damn good developer!',{type:'terminal'}, function (err, url) {
+//   console.log(url)
+// })
+
+// 2. Weather
+
+const axios = require("axios");
+
+const fetchWeatherData = async() =>{
+    try{
+        const response = await axios(
+            "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
+        );
+        console.log({response});
+
+        //Extract the relevant weather data from the response
+
+        const{latitude,longitude,...rest}=response.data;
+        return{rest};
+    } catch (error){
+        throw new Error("Failed to fetch weather data.");
+    }
+};
+
+// 3. Write a node.js application to convert the currency from one currency to another using package: current-converter-lt
+
+// const CC = require('currency-converter-lt')
+
+// let currencyConverter = new CC({from:"NRP", to:"JPY", amount:15000, isDecimalComma:true});
+
+// currencyConverter.convert().then((response) => {
+//     console.log(response); //or do something else
+// })
+
+// currencyConverter.convert(15000).then((response) => {
+//     console.log(response) //or do something else
+// })
+
+// currencyConverter.rates().then((response) => {
+//     console.log(response) //or do something else
+// })
